02b20dfc84e7ff726bdfaf241fd016875ce88cb3
Madhav Shukla <madhavshuklasr3@gmail.com>
2022-05-09 21:44:52 +0530

    Update in contract

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -2,4 +2,22 @@ pragma solidity ^0.5.16;
 
 contract Todo {
     uint256 public taskCount = 0;
+
+    struct Task{
+        uint id; 
+        string content ; 
+        bool completed ; 
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Madhav Shukla's todo list");
+    }
+
+    function createTask(string memory _content) public { 
+        taskCount++ ; 
+        tasks[taskCount] = Task(taskCount,_content,false) ; 
+    }
+
 }
