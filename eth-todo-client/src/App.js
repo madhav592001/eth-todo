fd37009d31438c74001420e0859a16f29cc75571
Madhav Shukla <madhavshuklasr3@gmail.com>
2022-06-01 22:12:35 +0530

    initialized react app and connected to metamask

---
+++ eth-todo-client/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect,useState} from 'react'
+import Web3 from 'web3'
+
+function App() {
+
+  const [account, setAccount] = useState();
+
+  useEffect(() => {
+    async function load() {
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+      const accounts = await web3.eth.requestAccounts();
+
+      setAccount(accounts[0]);
+    }
+
+    load();
+  }, []);
+  return (
+    <div className="App">
+    Your account is : {account}
+    </div>
+  );
+}
+
+export default App;
